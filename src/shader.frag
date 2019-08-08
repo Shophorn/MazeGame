e2c61ab945ba96e8607215c92931867b4658aab3
unknown <shophorn@protonmail.com>
2019-08-08 03:10:54 +0300

    First crappy lights

--- src/shader.frag
+++ src/shader.frag
@@ -2,14 +2,22 @@
 
 layout(location = 0) in vec3 fragColor;
 layout(location = 1) in vec2 fragTexCoord;
+layout(location = 2) in vec3 fragNormal;
 
 layout(location = 0) out vec4 outColor;
 
 layout(binding = 1) uniform sampler2D texSampler;
 
+
+const vec3 lightDirection = normalize(vec3(0.7, -1, 0.3));
+const float ambientIntensity = 0.15;
+
 void main()
 {
-	outColor.rgb = texture (texSampler, fragTexCoord).rgb * fragColor;
-	outColor.rgb = fragColor;
+	float intensity = dot(-lightDirection, fragNormal) + ambientIntensity;
+
+	outColor.rgb = texture (texSampler, fragTexCoord).rgb * fragColor *intensity;
+	outColor.rgb = fragNormal * 0.5 + 0.5;
+	outColor.rgb = fragColor * intensity;
 	outColor.a = 1.0;
 }
--- src/vertex_data.cpp
+++ src/vertex_data.cpp
@@ -4,6 +4,7 @@
 struct Vertex
 {
 	Vector3 position;
+	Vector3 normal;
 	Vector3 color;
 	Vector2 texCoord;
 
@@ -19,10 +20,10 @@ struct Vertex
 		return value;
 	}
 
-	static Array<VkVertexInputAttributeDescription, 3>
+	static Array<VkVertexInputAttributeDescription, 4>
 	GetAttributeDescriptions()
 	{
-		Array<VkVertexInputAttributeDescription, 3> value = {};
+		Array<VkVertexInputAttributeDescription, 4> value = {};
 
 		value[0].binding = 0;
 		value[0].location = 0;
@@ -32,12 +33,17 @@ struct Vertex
 		value[1].binding = 0;
 		value[1].location = 1;
 		value[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-		value[1].offset = offsetof(Vertex, color);	
+		value[1].offset = offsetof(Vertex, normal);
 
 		value[2].binding = 0;
 		value[2].location = 2;
-		value[2].format = VK_FORMAT_R32G32_SFLOAT;
-		value[2].offset = offsetof(Vertex, texCoord);
+		value[2].format = VK_FORMAT_R32G32B32_SFLOAT;
+		value[2].offset = offsetof(Vertex, color);	
+
+		value[3].binding = 0;
+		value[3].location = 3;
+		value[3].format = VK_FORMAT_R32G32_SFLOAT;
+		value[3].offset = offsetof(Vertex, texCoord);
 
 		return value;
 	}	
@@ -95,6 +101,12 @@ GenerateMap()
 			result.vertices[vertexIndex + 2].color = color;
 			result.vertices[vertexIndex + 3].color = color;
 
+			Vector3 normal = {0, 1, 0};
+			result.vertices[vertexIndex].normal = normal;
+			result.vertices[vertexIndex + 1].normal = normal;
+			result.vertices[vertexIndex + 2].normal = normal;
+			result.vertices[vertexIndex + 3].normal = normal;
+
 			result.indices[triangleIndex] = vertexIndex;
 			result.indices[triangleIndex + 1] = vertexIndex + 2;
 			result.indices[triangleIndex + 2] = vertexIndex + 1;
@@ -143,6 +155,12 @@ LoadModel(const char * model_path)
 			};
 
 			vertex.color = {0.8f, 1.0f, 1.0f};
+
+			vertex.normal = {
+				attrib.normals[3 * index.normal_index + 0],
+				attrib.normals[3 * index.normal_index + 1],
+				attrib.normals[3 * index.normal_index + 2]
+			};
 
 			result.vertices.push_back(vertex);
 			result.indices.push_back(result.indices.size());
