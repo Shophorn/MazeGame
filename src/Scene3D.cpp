9bfd29f94f74d47954817ab84415a1a0171b402e
Leo Tamminen <32251076+Shophorn@users.noreply.github.com>
2020-09-04 13:18:43 +0300

    Git test message

--- src/Scene3D.cpp
+++ src/Scene3D.cpp
@@ -4,6 +4,11 @@ shophorn @ internet
 
 Scene description for 3d development scene
 =============================================================================*/
+
+/*
+git test
+*/
+
 #include "CharacterMotor.cpp"
 #include "PlayerController3rdPerson.cpp"
 #include "FollowerController.cpp"
